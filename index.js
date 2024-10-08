7e1f53fb753925ba53508327ad6bcc786fce1610
Felix Wege <fwege@cc.systems>
2024-10-08 17:18:15 +0200

    feat: use correct message type

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 export function myFn() {
   console.log("Hello, World!");
-  window.addEventListener('event', (event) => {
+  window.addEventListener('message', (event) => {
     console.log("received event", event);
   })
 }
